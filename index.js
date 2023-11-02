15d6384741a19f885cba3a6e0f2b1d4bb9a989a9
Barshon <barshonweb@gmail.com>
2023-11-02 23:27:50 +0600

    data loaded in server from json file

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express= require('express');
 const app= express();
 const port=process.env.PORT || 5000;
 
+const cors= require('cors');
+app.use(cors());
 
+const chefs= require('./data/chefData.json')
 app.get('/', (req,res)=>{
-    res.send ('chefs infos');
+    res.send (chefs);
 })
 
 app.listen(port, ()=>{
